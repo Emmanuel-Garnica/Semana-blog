1844a756db330425d521abf9767ad01fef83ed1a
Emmanuel Garnica <edgarnicap95@gmail.com>
2024-05-24 14:19:30 -0500

    Feature(Posts): Get posts information.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import { useState, useEffect } from 'react';
+import { apiInstance } from './api';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiInstance.get('post');
+        console.log(response.data.data)
+        setData(response.data.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
-    <>
-      <main>
-        This is the blog
-      </main>
-    </>
-  )
-}
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const apiInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    'app-id': import.meta.env.VITE_API_ID
+  }
+})
